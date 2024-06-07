f96858c0a34d2251418fd766df0ebbdcc3993817
Disha <disha81100@gmail.com>
2024-06-07 14:47:46 -0700

    Bug Fixed after changes.

--- CheckListApp/Test.swift
+++ CheckListApp/Test.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+struct Question {
+    func fizzBuzz(n: Int) -> String {
+        if (n % 3 == 0 && n % 5 == 0) {
+            return "FizzBuzz"
+        } else if (n % 3 == 0){
+            return "Fizz"
+        } else if (n % 5 == 0){
+            return "Buzz"
+        } else {
+            return "\(n)"
+        }
+    }
+}
--- CheckListAppTests/CheckListAppTests.swift
+++ CheckListAppTests/CheckListAppTests.swift
@@ -10,27 +10,33 @@ import XCTest
 
 final class CheckListAppTests: XCTestCase {
 
+    var objFizzBuzz: Question?
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        objFizzBuzz = Question()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        objFizzBuzz = nil
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func testFizz(){
+        let result = objFizzBuzz?.fizzBuzz(n:9)
+        XCTAssertEqual(result, "Fizz")
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testBuzz(){
+        let result = objFizzBuzz?.fizzBuzz(n:10)
+        XCTAssertEqual(result, "Buzz")
     }
-
+    
+    func testFizzBuzz(){
+        let result = objFizzBuzz?.fizzBuzz(n:15)
+        XCTAssertEqual(result, "FizzBuzz")
+    }
+    
+    func testInvalid(){
+        let result = objFizzBuzz?.fizzBuzz(n:14)
+        XCTAssertEqual(result, "14")
+    }
 }
